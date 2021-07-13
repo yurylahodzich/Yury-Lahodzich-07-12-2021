e10f1151855fee7953412b08d898b65efb2bcbfb
yury lahodzich <yurylahodzich@getpinwheel.com>
2021-07-13 12:36:18 +0300

    backend v1

---
+++ backend/chat/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from chat.views import MessageViewSet
+
+
+router = DefaultRouter()
+router.register(r'message', MessageViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,2 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import GenericViewSet
 
+from chat.models import Message
+from chat.serializer import MessageSerializer
+
+
+class MessageViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin,
+                     mixins.ListModelMixin,
+                     GenericViewSet):
+    """
+    A simple ViewSet for viewing and editing accounts.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = (AllowAny,)
