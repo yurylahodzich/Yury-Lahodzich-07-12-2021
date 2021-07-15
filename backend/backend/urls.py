ab0e4c1df16bc66d01693c3b914b238b525cdb6a
yury lahodzich <yurylahodzich@getpinwheel.com>
2021-07-15 18:14:52 +0300

    static fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,13 @@
+from django.urls import include, path, re_path
+from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chat.urls'))
-]
+                  path('admin/', admin.site.urls),
+                  path('api/', include('chat.urls')),
+                  re_path(r'^media/(?P<path>.)$', serve, {'document_root': settings.MEDIA_ROOT}),
+                  re_path(r'^static/(?P<path>.)$', serve, {'document_root': settings.STATIC_ROOT}),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                         document_root=settings.STATIC_ROOT)
