e3b2f8a571b92ab079c8f6c2fd95ecb3ba20af1c
yury lahodzich <yurylahodzich@getpinwheel.com>
2021-07-15 18:01:22 +0300

    nginx update

--- frontend/chat/src/http-common.js
+++ frontend/chat/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://zymzom.tk/api/",
+  baseURL: "https://zymzom.tk/api/",
   headers: {
     "Content-type": "application/json"
   }
